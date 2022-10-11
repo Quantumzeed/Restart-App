2f72bfa79c401323c4d7673dc2b509ff4a6d3a22
Quantumzeed <87402893+Quantumzeed@users.noreply.github.com>
2022-10-11 13:55:15 +0700

    first

---
+++ Students/Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Quantum on 20/7/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
